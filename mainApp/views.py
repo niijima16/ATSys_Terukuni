cfc7598ba992a0e0f87f050d17340cb0d2965fa6
niijima16 <niijima.16@gmail.com>
2024-05-26 21:33:31 +0900

    Initial commit

---
+++ mainApp/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render,HttpResponse,redirect
+
+# Create your views here.
+def homePage(request):
+    if request.method == "GET":
+        return render(request,"homePage.html")
+
+    account = request.POST.get("user")
+    password = request.POST.get("password")
+
+    if account == 'root@levels.co.jp' and password == "123":
+        return render(request,"topPage.html")
+
+def topPage(request):
+    return render(request,"topPage.html")
