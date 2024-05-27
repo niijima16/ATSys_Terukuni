6bd6bf36f9e688389d1d2bdbdbcc4e92b5a7115f
niijima16 <niijima.16@gmail.com>
2024-05-27 22:40:53 +0900

    fixsomethin

--- mainApp/models.py
+++ mainApp/models.py
@@ -1,11 +1,21 @@
 from django.db import models
 
 # Create your models here.
-class uerInfo(models.Model):
-    name = models.CharField(max_length=32)
-    passwd = models.CharField(max_length=64)
-    age = models.IntegerField(default=20)
-    size = models.IntegerField(null=True, blank=True)
+class user_master(models.Model):
+    user_id = models.AutoField(primary_key=True)
+    pwd = models.CharField(max_length=256)
+    name = models.CharField(max_length=256)
+    age = models.PositiveSmallIntegerField()  # charint(4)に対応するフィールド
+    GENDER_CHOICES = [
+        ('M', 'Male'),
+        ('F', 'Female'),
+        ('O', 'Other'),
+    ]
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+    phone_number = models.CharField(max_length=11)  # charint(11)に対応するフィールド
+    joined = models.DateField()
+    department_id = models.IntegerField()
+    department = models.CharField(max_length=256)
 
 
 class Department(models.Model):
