a3ee5183db0ebcd05073e0534e318a97ca7d1eb2
niijima16 <niijima.16@gmail.com>
2024-06-22 21:36:49 +0900

    add the automatic migrate script

--- run.sh
+++ run.sh
@@ -10,6 +10,7 @@ echo "MySQL database is ready!"
 # マイグレーションを実行
 python manage.py makemigrations --no-input
 python manage.py migrate --no-input
+python manage.py create_db_users
 
 # Djangoサーバーを起動
 python manage.py runserver 0.0.0.0:8000
