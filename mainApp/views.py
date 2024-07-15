93361e5dd8a7e5f72e1bb661d7624032caf18570
terukuni_sho <niijima.16@gmail.com>
2024-07-15 10:09:15 +0900

    0715

--- mainApp/views.py
+++ mainApp/views.py
@@ -1,39 +1,3 @@
-# from django.shortcuts import render, redirect
-# from django.contrib.auth.hashers import check_password
-# from .models import User_Master, COMPANY
-# from .forms import UserMasterFormSet
-
-# def homePage(request):
-#     error_message = None
-#     formset = UserMasterFormSet(instance=COMPANY())  # フォームセットを初期化
-
-#     if request.method == 'POST':
-#         # ログイン認証
-#         account = request.POST.get('user')
-#         password = request.POST.get('password')
-        
-#         if account == 'root@levels.co.jp' and check_password(password, 'root'):
-#             # 認証成功時の処理
-#             formset = UserMasterFormSet(request.POST, instance=COMPANY())  # 親オブジェクトのインスタンスをフォームセットに渡す
-#             if formset.is_valid():
-#                 formset.save()
-#                 return redirect('topPage')  # urlpatternsで定義した'topPage'という名前のビューにリダイレクトする
-#         else:
-#             # 認証失敗時の処理
-#             error_message = 'ログインに失敗しました。有効なアカウント情報を入力してください。'
-
-#     return render(request, 'HomePage.html', {
-#         'formset': formset,
-#         'error_message': error_message,
-#     })
-
-# def topPage(request):
-#     return render(request, 'topPage.html')
-
-# test01@levels.co.jp
-# test01　
-
-
 from django.shortcuts import render, redirect
 from .models import User_Master
 from .forms import LoginForm
