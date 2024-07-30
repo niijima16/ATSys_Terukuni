932ded0f200c743c370bcec39da5e3d3b3762f1c
terukuni_sho <niijima.16@gmail.com>
2024-07-30 23:04:04 +0900

    update 2 user(BOSS-PG,Skadi-平), leavetype,leaveRequest at django-admin page

--- mainApp/models/__init__.py
+++ mainApp/models/__init__.py
@@ -2,3 +2,5 @@ from .user_master import User_Master
 from .leave_type import LeaveType
 from .leave_request import LeaveRequest
 from .attendance import Attendance
+from .time_sheet import TimeSheet
+from .time_shift import Shift
